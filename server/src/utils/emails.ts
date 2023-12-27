04fb93354eb1fcd653f4ae41a29c4df09d3b6cb1
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-27 17:15:15 -0300

    Implement email functionalities

---
+++ server/src/utils/emails.ts
@@ -0,0 +1,48 @@
+import nodemailer from 'nodemailer';
+
+type UserInfo = {
+  fullName?: string;
+  email: string;
+};
+
+export class Email {
+  to: string;
+  from: string;
+  name?: string;
+  url: string;
+
+  constructor(userInfo: UserInfo, url: string) {
+    this.to = userInfo.email;
+    this.name = userInfo.fullName;
+    this.url = url;
+    this.from = `Arkides <arkides@gmail.com.ar>`;
+  }
+
+  newTransport() {
+    //Brevo provider
+    return nodemailer.createTransport({
+      auth: {
+        user: process.env.BREVO_EMAIL,
+        pass: process.env.BREVO_PASSWORD,
+      },
+      port: Number(process.env.BREVO_PORT),
+      host: process.env.BREVO_HOST,
+    });
+  }
+
+  async send(html: string, subject: string) {
+    const emailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+    };
+
+    await this.newTransport().sendMail(emailOptions);
+  }
+
+  verificationCode() {
+    console.log('sent email');
+    this.send('<h1>Test</h1>', 'Authentication code');
+  }
+}
