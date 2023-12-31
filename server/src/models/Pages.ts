66ef6e95b1a26252a9a9adb76b255ac1cf5b9f52
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-31 19:48:57 -0300

    Create Pages model

---
+++ server/src/models/Pages.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const contentSchema = new mongoose.Schema({
+  //Common for all contents
+  title: String,
+  content: String,
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  //For tasks only
+  status: {
+    type: String,
+    enum: ['pending', 'progress', 'finished'],
+    default: 'pending',
+  },
+  dueDate: Date,
+  //For notes only
+  style: String,
+});
+
+const pagesSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Pages must have a name.'],
+  },
+  icon: {
+    type: String,
+  },
+  type: {
+    type: String,
+    enum: ['task', 'notes'],
+    required: [true, 'Pages must have a type.'],
+  },
+  content: {
+    type: [contentSchema],
+  },
+});
+
+export const Page = mongoose.model('Page', pagesSchema);
