e6b9a3ab31fa8fa6363a43adcfb549b783115d40
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-02 10:43:10 -0300

    Update chats model

--- server/src/models/Chat.ts
+++ server/src/models/Chat.ts
@@ -2,21 +2,20 @@ import mongoose from 'mongoose';
 
 export const chatSchema = new mongoose.Schema({
   name: {
-    //Name is only required when chat is part of a project OR if it is a group chat
-    required: function () {
-      return this.projectId.length >= 1 || this.members.length > 2;
-    },
     type: String,
     minLength: [6, 'Chat name must have at least 6 characters.'],
     maxLength: [40, `Chat name can't have more than 40 characters.`],
-    trim: true,
   },
   members: {
     required: [true, 'Chats must have members array.'],
     type: [mongoose.Schema.ObjectId],
     ref: 'User',
-    minItems: [2, 'Must have at least 2 members.'],
+    minItems: [1, 'Must have at least 1 member in addition to you.'],
     maxItems: [15, `Can't have more than 15 members.`],
+  },
+  type: {
+    type: String,
+    enum: ['single', 'group'],
   },
 });
 
