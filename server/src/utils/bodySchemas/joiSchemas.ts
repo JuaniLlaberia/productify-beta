263feaa664d0d35bcd97d2b254531e20d67c77c1
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-02 15:46:05 -0300

    Create joi validation schemas

---
+++ server/src/utils/bodySchemas/joiSchemas.ts
@@ -0,0 +1,70 @@
+import Joi from 'joi';
+
+export const userIdSchema = Joi.object<{ userId: string }>({
+  userId: Joi.string().required(),
+});
+
+export const emailSchema = Joi.object<{ email: string }>({
+  email: Joi.string().email().required(),
+});
+
+export const passwordSchema = Joi.object<{
+  password: string;
+  confirmedPassword: string;
+}>({
+  password: Joi.string().required(),
+  confirmedPassword: Joi.string().required(),
+});
+
+export const chatSchema = Joi.object<{ members: string[]; name: string }>({
+  members: Joi.array().min(1).required(),
+  name: Joi.when('members', {
+    is: Joi.array().min(2),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+});
+
+export const projectSchema = Joi.object<{
+  name: string;
+}>({
+  name: Joi.string().min(4).max(20).required(),
+});
+
+export const eventSchema = Joi.object<{
+  date: Date;
+  content: string;
+  style: string;
+}>({
+  content: Joi.string().min(4).max(150).required(),
+  date: Joi.date().required(),
+  style: Joi.string(),
+});
+
+export const pageSchema = Joi.object<{
+  pageType: 'task' | 'notes';
+  name: string;
+}>({
+  name: Joi.string().min(4).max(20).required(),
+  pageType: Joi.string().valid('task', 'notes').required(),
+});
+
+export const contentSchema = Joi.object<{
+  title: string;
+  content: string;
+  status: 'pending' | 'progress' | 'finished';
+  dueDate: Date;
+  style: string;
+}>({
+  title: Joi.string().min(4).max(20).required(),
+  content: Joi.string().required(),
+  status: Joi.string().valid('pending', 'progress', 'finished'),
+  dueDate: Joi.date(),
+  style: Joi.string(),
+});
+
+export const contentTypeSchema = Joi.object<{
+  status: 'pending' | 'progress' | 'finished';
+}>({
+  status: Joi.string().valid('pending', 'progress', 'finished').required(),
+});
