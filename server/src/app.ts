e2f6860a1767e7cb5f5c21b60f561036ef0fde91
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-02 15:45:47 -0300

    Add validation to email auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,3 @@
-// const express = require('express');
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
@@ -19,6 +18,8 @@ import { router as pageRouter } from './routes/pageRoutes';
 import { router as chatRouter } from './routes/chatRoutes';
 import { errorHandler } from './controllers/errorController';
 import { CustomError } from './utils/emailTemplates/error';
+import { validateBody } from './middleware/validateBody';
+import { emailSchema } from './utils/bodySchemas/joiSchemas';
 
 export const app = express();
 
@@ -37,12 +38,12 @@ app.set('trust proxy', 1);
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
-app.use(compression());
+app.use(compression({ level: 6, threshold: 0 }));
 
 //Auth routes
 app.get('/api/v1/oauth/google', googleAuthHandler);
 app.get('/api/v1/oauth/github', githubAuthHandler);
-app.post('/api/v1/auth/email', emailAuthHandler);
+app.post('/api/v1/auth/email', validateBody(emailSchema), emailAuthHandler);
 
 //App routes
 app.use('/api/v1/auth', authRouter);
