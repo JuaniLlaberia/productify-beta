c191c9bc232f09175e9a1cfa1b749a506d56ef79
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-04 12:48:35 -0300

    Create user data context

---
+++ client/src/context/UserContext.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, createContext, useContext } from 'react';
+import { AuthType, useGetAuth } from '../features/authentication/useGetAuth';
+
+type ContextType = {
+  user?: AuthType;
+};
+
+const UserContext = createContext<ContextType>({});
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const { userData, isLoading } = useGetAuth();
+
+  if (isLoading) return <h1>Loading user data...</h1>;
+
+  return (
+    <UserContext.Provider value={{ user: userData }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => useContext(UserContext);
