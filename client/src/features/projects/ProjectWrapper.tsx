05da9e433bbaca4fc4ee10441cb603858d8270fb
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-05 18:36:21 -0300

    Delete unnecessary components

--- client/src/features/projects/ProjectWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react';
-
-const ProjectWrapper = ({ children }: { children: ReactNode }) => {
-  return <section className='flex w-full'>{children}</section>;
-};
-
-export default ProjectWrapper;
