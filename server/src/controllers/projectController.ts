bc8b97c7912c40b1573af26dde838cfc71841ac2
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-06 14:55:10 -0300

    Update projects controller

--- server/src/controllers/projectController.ts
+++ server/src/controllers/projectController.ts
@@ -33,7 +33,7 @@ export const getProjects = catchAsyncError(
     //Pagination
     if (req.query.page) {
       const page = Number(req.query.page) || 1;
-      const limit = Number(req.query.limit) || 10;
+      const limit = Number(req.query.limit) || 5;
       const skip = (page - 1) * limit;
 
       projectsQuery.skip(skip).limit(limit);
@@ -72,11 +72,13 @@ export const createProject = catchAsyncError(
       { $inc: { projectsLeft: -1 } }
     );
 
-    await Promise.all([projectPromise, userPromise]);
+    const [newProject] = await Promise.all([projectPromise, userPromise]);
 
-    res
-      .status(201)
-      .json({ status: 'success', message: 'Project created successfully.' });
+    res.status(201).json({
+      status: 'success',
+      message: 'Project created successfully.',
+      data: { projectId: newProject._id },
+    });
   }
 );
 
