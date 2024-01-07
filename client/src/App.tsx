75a3a7c351833ec14d900a38592fbcdfb2bdabe0
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-06 21:13:47 -0300

    Update client routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LandingPage from './pages/LandingPage';
 import ProtectRoutes from './wrappers/ProtectRoutes';
-import { LoginPage } from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
+import AuthPage from './pages/AuthPage';
 import { UserProvider } from './context/UserContext';
 import HomePage from './pages/HomePage';
 import { Toaster } from 'sonner';
 import ProjectPage from './pages/ProjectPage';
 import ProjectFormPage from './pages/ProjectFormPage';
+import LoginPage from './pages/LoginPage';
+import AuthLayout from './wrappers/AuthLayout';
 
 const router = createBrowserRouter([
   {
@@ -17,12 +18,17 @@ const router = createBrowserRouter([
     element: <LandingPage />,
   },
   {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/signup',
-    element: <SignUpPage />,
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/auth',
+        element: <AuthPage />,
+      },
+    ],
   },
   {
     element: <ProtectRoutes />,
