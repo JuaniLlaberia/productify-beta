9d32e4bb42d33a79b20a7319d01afb42e6ce4b92
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-08 19:23:19 -0300

    Add project provider

--- client/src/pages/ProjectPage.tsx
+++ client/src/pages/ProjectPage.tsx
@@ -1,16 +1,19 @@
 import Project from '../features/projects/ProjectInfoLayout';
 import ProjectSidebar from '../features/projects/ProjectSidebar';
+import { ProjectProvider } from '../context/ProjectContext';
 
 const ProjectPage = () => {
   return (
-    <Project>
-      <Project.Sidebar>
-        <ProjectSidebar />
-      </Project.Sidebar>
-      <Project.Content>
-        <p>PAGE CONTENT</p>
-      </Project.Content>
-    </Project>
+    <ProjectProvider>
+      <Project>
+        <Project.Sidebar>
+          <ProjectSidebar />
+        </Project.Sidebar>
+        <Project.Content>
+          <p>PAGE CONTENT</p>
+        </Project.Content>
+      </Project>
+    </ProjectProvider>
   );
 };
 
