af848a45587742d71dbfb4407ba05aca686ec256
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-09 15:26:57 -0300

    Update tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,3 +1,5 @@
+import scrollbarPlugin from 'tailwind-scrollbar';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -7,11 +9,15 @@ export default {
       colors: {
         'bg-light-1': '#f5f4f0',
         'bg-light-2': '#fcfcfa',
-        'bg-light-3': '#eeebe7',
+        'bg-light-3': '#ffffff',
+        'bg-light-hover': '#dddbdb1a',
+        'bg-light-hover-2': '#9493931a',
         'bg-light-contrast': '#2B2829',
         'text-light-1': '#1F1C1C',
         'text-light-2': '#575757dd',
         'border-light': '#0c0b0c25',
+        'scroll-light': '#e4e4e47e',
+        'scroll-light-hover': '#ccc9c96c',
         'bg-dark-1': '#252324',
         'bg-dark-2': '#201e1e',
         'bg-dark-3': '#313030',
@@ -23,5 +29,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbarPlugin({})],
 };
