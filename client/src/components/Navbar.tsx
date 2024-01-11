c3438b363ddca77b867c6cf380434ed02ac1f7f1
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-11 15:56:41 -0300

    Update navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import UserDropdownProfile from '../features/user/UserDropdownProfile';
 const Navbar = ({ includeLogo }: { includeLogo?: boolean }) => {
   return (
     <nav
-      className={`h-20 flex ${
+      className={`h-20 sticky top-0 flex ${
         includeLogo ? 'justify-between' : 'justify-end'
-      } items-center px-6 py-3`}
+      } items-center px-6 py-3 bg-bg-light-1`}
     >
       <Link to='/home'>{includeLogo ? 'LOGO' : null}</Link>
       <UserDropdownProfile />
