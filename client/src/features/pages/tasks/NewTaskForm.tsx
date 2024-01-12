46a279f4093eb9330efb9a610223c4955de97244
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-12 12:28:23 -0300

    Finish create task form

--- client/src/features/pages/tasks/NewTaskForm.tsx
+++ client/src/features/pages/tasks/NewTaskForm.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form';
+import { HiOutlineCalendarDays, HiOutlineTag } from 'react-icons/hi2';
 
 import Button from '../../../components/Button';
+import SelectSingle from '../../../components/SelectCustom';
 import { useCreateContent } from '../useCreateContent';
 
 const NewTaskForm = ({
@@ -10,13 +12,13 @@ const NewTaskForm = ({
   onClose?: () => void;
   status: 'pending' | 'progress' | 'finished';
 }) => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue, watch } = useForm();
 
   const { addContent, isLoading } = useCreateContent();
 
-  const handleNewTask = handleSubmit(({ title, content }) => {
+  const handleNewTask = handleSubmit(({ title, content, importance, tag }) => {
     addContent(
-      { title, content, status },
+      { title, content, status, importance: importance.value, tag: tag.value },
       {
         onSuccess: () => {
           if (onClose) onClose();
@@ -30,7 +32,7 @@ const NewTaskForm = ({
       <input
         disabled={isLoading}
         {...register('title', { required: 'required' })}
-        className='font-semibold text-5xl w-full border-none outline-none placeholder:text-text-light-2 placeholder:opacity-80'
+        className='font-semibold text-xl xl:text-5xl w-full border-none outline-none placeholder:text-text-light-2 placeholder:opacity-80'
         placeholder='Untitled'
       />
       <textarea
@@ -39,7 +41,41 @@ const NewTaskForm = ({
         className='w-full h-[200px] resize-none mt-3 outline-none border-none lg:scrollbar-thin lg:scrollbar-thumb-scroll-light hover:lg:scrollbar-thumb-scroll-light-hover'
         placeholder='Describe what needs to be done'
       />
-      <div>//TAG //CALENDAR</div>
+
+      <SelectSingle
+        options={[
+          { label: 'Urgent', value: 'urgent' },
+          { label: 'Important', value: 'important' },
+          { label: 'Moderate', value: 'moderate' },
+        ]}
+        selectedOption={watch('importance') || ''}
+        onChange={option => {
+          setValue('importance', option);
+        }}
+        removeBorders
+        placeholder='Select importance'
+        icon={<HiOutlineCalendarDays />}
+      />
+      <SelectSingle
+        options={[
+          { label: 'New Feature', value: 'feature' },
+          { label: 'Fix', value: 'fix' },
+          { label: 'Refactor', value: 'refactor' },
+          { label: 'Testing', value: 'testing' },
+          { label: 'Documentation', value: 'documentation' },
+          { label: 'Integration', value: 'integration' },
+          { label: 'Deployment', value: 'deployment' },
+          { label: 'Maintenance', value: 'maintenance' },
+        ]}
+        selectedOption={watch('tag') || ''}
+        onChange={option => {
+          setValue('tag', option);
+        }}
+        removeBorders
+        placeholder='Select task tag'
+        icon={<HiOutlineTag />}
+      />
+
       <div className='flex justify-between mt-10'>
         <Button
           disabled={isLoading}
