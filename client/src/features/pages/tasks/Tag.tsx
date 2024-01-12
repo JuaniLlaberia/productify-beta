e954b733d207310fc0988e8d82f480195a7f9dc7
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-12 12:28:35 -0300

    Refactor tag component

--- client/src/features/pages/tasks/Tag.tsx
+++ client/src/features/pages/tasks/Tag.tsx
@@ -1,19 +1,19 @@
-const Tag = ({ tag }: { tag: 'pending' | 'progress' | 'finished' }) => {
+const Tag = ({
+  label,
+  color,
+}: {
+  label: string;
+  color: 'red' | 'blue' | 'green' | 'purple' | 'yellow' | 'gray' | 'orange';
+}) => {
+  console.log(color);
+
   return (
     <span
       className={`
-        ${
-          tag === 'pending'
-            ? 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300'
-            : tag === 'progress'
-            ? 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300'
-            : tag === 'finished'
-            ? 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300'
-            : ''
-        }
+        bg-${color}-100 text-${color}-800 dark:bg-${color}-900 dark:text-${color}-300
          text-sm xl:text-base font-semibold me-2 px-4 py-1 rounded-lg shadow-sm capitalize`}
     >
-      {tag}
+      {label}
     </span>
   );
 };
