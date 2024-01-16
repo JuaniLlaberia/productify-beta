a6465f7e4885237e089cd74f5f47e1e6585e20b3
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-01-16 16:44:02 -0300

    Fix stying bug

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -36,7 +36,7 @@ const Input = ({
           id={label}
           className={`w-full p-2 py-2 lg:py-2.5 ${
             icon ? 'pl-10' : 'lg:px-3'
-          } my-1 border bg-transparent text-text-light-1 dark:text-text-dark-1 md:hover:bg-bg-light-hover ${
+          } my-1 border bg-bg-light-3 dark:bg-bg-dark-2 text-text-light-1 dark:text-text-dark-1 md:hover:bg-bg-light-hover ${
             errorMsg
               ? 'border-red-400 outline-red-400'
               : 'border-border-light outline-bg-light-contrast dark:border-border-dark dark:outline-bg-dark-contrast'
