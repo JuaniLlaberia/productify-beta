5dada22aa54ab35e0122860ac3e8aaf92d6ddbdc
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-02-06 16:28:49 -0300

    Update types

--- client/src/types/extraTypes.ts
+++ client/src/types/extraTypes.ts
@@ -9,3 +9,12 @@ export type PasswordsType = {
   password: string;
   confirmedPassword: string;
 };
+
+export type ColorsType =
+  | 'red'
+  | 'blue'
+  | 'green'
+  | 'purple'
+  | 'yellow'
+  | 'gray'
+  | 'orange';
