cb21bf92c97691bb04dbe32bd8bf6c1f4bf52ace
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-02-06 16:28:22 -0300

    Refactor pages/tasks in the server

--- server/src/routes/pageRoutes.ts
+++ server/src/routes/pageRoutes.ts
@@ -1,21 +1,26 @@
 import express from 'express';
+
 import { authProtect } from '../controllers/authController';
 import { adminRestriction } from '../controllers/projectController';
 import {
-  addContent,
+  addTask,
   belongsToUser,
-  changeStatusContent,
+  changeStatusTask,
   createPage,
-  deleteContent,
+  deleteTask,
   deletePage,
   getPage,
-  updateContent,
+  updateTask,
+  addColumn,
+  deleteColumn,
 } from '../controllers/pageController';
+
 import { validateBody } from '../middleware/validateBody';
 import {
-  contentSchema,
+  taskSchema,
   contentTypeSchema,
   pageSchema,
+  columnSchema,
 } from '../utils/bodySchemas/joiSchemas';
 
 export const router = express.Router();
@@ -25,20 +30,22 @@ router.use(authProtect);
 //Pages actions
 router.route('/:pageId').get(getPage);
 router
-  .route('/new/:projectId')
+  .route('/:projectId/new-page')
   .post(adminRestriction, validateBody(pageSchema), createPage);
-router.route('/delete/:pageId/:projectId').delete(adminRestriction, deletePage);
+router.route('/:projectId/delete/:pageId').delete(adminRestriction, deletePage);
 
-//Content actions
+//Columns actions
 router
-  .route('/:pageId/content/new')
-  .patch(validateBody(contentSchema), addContent);
+  .route('/:pageId/column/new')
+  .patch(validateBody(columnSchema), addColumn);
 router
-  .route('/:pageId/content/update/:contentId')
-  .patch(belongsToUser, updateContent);
+  .route('/:pageId/column/delete/:columnId')
+  .delete(adminRestriction, deleteColumn);
+
+//Tasks actions
+router.route('/:pageId/task/new').patch(validateBody(taskSchema), addTask);
+router.route('/:pageId/task/update/:taskId').patch(belongsToUser, updateTask);
 router
-  .route('/:pageId/content/update-status/:contentId')
-  .patch(validateBody(contentTypeSchema), changeStatusContent);
-router
-  .route('/:pageId/content/delete/:contentId')
-  .delete(belongsToUser, deleteContent);
+  .route('/:pageId/task/update-status/:taskId')
+  .patch(validateBody(contentTypeSchema), changeStatusTask);
+router.route('/:pageId/task/delete/:taskId').delete(belongsToUser, deleteTask);
