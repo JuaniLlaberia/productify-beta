66c306c576e2cf9f5df611f218a6e9f9d058b8dd
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-02-16 15:53:27 -0300

    Update utils function

--- server/src/utils/userImgGenerator.ts
+++ server/src/utils/userImgGenerator.ts
@@ -6,13 +6,19 @@ const getRandomColor = () => {
 };
 
 export const createImage = async (initial: string) => {
-  //Get random bg color
+  // Get random bg color
   const color = getRandomColor();
+  const fontFamily = 'Arial';
 
-  //Create + optimize image
-  const svgImg = `<svg width="150" height="150" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="${color}"/><text x="50%" y="50%" font-size="60" dy=".3em" fill="#fff" text-anchor="middle">${initial.toUpperCase()}</text></svg>`;
+  const svgImg = `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="${color}"/>
+    <text x="50%" y="50%" font-size="80" dy=".3em" fill="#fff" text-anchor="middle" font-family="${fontFamily}">
+      ${initial.toUpperCase()}
+    </text>
+  </svg>`;
+
   const imgBuffer = await sharp(Buffer.from(svgImg))
-    .resize(150, 150)
+    .resize(200, 200)
     .webp({ quality: 90 })
     .toBuffer();
 
