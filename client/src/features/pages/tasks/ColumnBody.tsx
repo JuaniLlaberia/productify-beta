5a10cfc09ed26fb5e5105d793464eee6f73b657f
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-02-17 19:51:48 -0300

    Update tasks components

--- client/src/features/pages/tasks/ColumnBody.tsx
+++ client/src/features/pages/tasks/ColumnBody.tsx
@@ -1,15 +1,14 @@
 import { HiOutlinePlus } from 'react-icons/hi2';
 
 import TaskCard from './TaskCard';
-import Modal from '../../../components/Modal';
 import type { PageTaskType } from '../../../types/pagesTypes';
+import { DialogTrigger } from '../../../components/Dialog';
 
 type ColumnType = {
-  id: string;
   tasks: PageTaskType[];
 };
 
-const ColumnBody = ({ id, tasks }: ColumnType) => {
+const ColumnBody = ({ tasks }: ColumnType) => {
   return (
     <section className='w-full'>
       <ul className='flex flex-col gap-2 py-2 px-2 min-w-[325px]'>
@@ -22,14 +21,14 @@ const ColumnBody = ({ id, tasks }: ColumnType) => {
           ))
         ) : (
           <>
-            <Modal.Open windowId={`new-task-modal-${id}`}>
+            <DialogTrigger>
               <button
                 aria-label='Open modal'
                 className='flex items-center gap-2 w-full text-lg font-semibold text-text-light-2 dark:text-text-dark-2 md:hover:bg-bg-light-hover-2 p-1 rounded-lg transition-colors'
               >
                 <HiOutlinePlus size={18} /> Add
               </button>
-            </Modal.Open>
+            </DialogTrigger>
           </>
         )}
       </ul>
