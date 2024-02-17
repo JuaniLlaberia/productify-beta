4bdd8d477a5f147451d9b926c837cd989e0ea720
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-02-17 19:54:11 -0300

    Update landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,7 +1,17 @@
-import { createGoogleAuthUrl } from '../utils/createGoogleAuthUrl';
+import LandingNavbar from '../features/landingPage/LandingNavbar';
+import LandingHeader from '../features/landingPage/LandingHeader';
+import LandingCards from '../features/landingPage/LandingCards';
+import LandingFooter from '../features/landingPage/LandingFooter';
 
 const LandingPage = () => {
-  return <a href={createGoogleAuthUrl()}>LandingPage</a>;
+  return (
+    <main className='h-full min-h-[100dvh] w-full bg-bg-light-2 dark:bg-bg-dark-1'>
+      <LandingNavbar />
+      <LandingHeader />
+      <LandingCards />
+      <LandingFooter />
+    </main>
+  );
 };
 
 export default LandingPage;
